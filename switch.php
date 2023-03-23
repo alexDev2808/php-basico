fd646deb53ba4986b9209a379b559d1d125de040
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-23 17:21:05 -0600

    Switch en php

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+
+$numMichi = 4;
+
+
+switch ($numMichi) {
+    case 1: 
+        echo "Su numero favorito es el 9";
+        break;
+    case 4: 
+        echo "Su numero favorito es el 30";
+        break;
+    default: 
+        echo "Ese michi no existe!";
+}
+
+echo "\n";
+
+?>
