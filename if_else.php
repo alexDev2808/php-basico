60ff8a80b467f3871b854a44e0dc231830a6d788
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-23 17:11:25 -0600

    If else en php

---
+++ if_else.php
@@ -0,0 +1,16 @@
+<?php
+
+$asientos_disponibles = 0;
+$es_hijo_de_Tony_Stark = true;
+
+if($asientos_disponibles > 0) 
+    echo "Puedes ver la pelicula de Spider-Man";
+else if ($es_hijo_de_Tony_Stark)
+    echo "No te creo pero Puedes ver la pelicula";
+else
+    echo "Lo sentimos, no has asientos disponibles";
+
+
+echo "\n";
+
+?>
