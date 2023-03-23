876bd58ad4a033e4ad2cfd84a43fed1d7a1e6e3e
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-23 15:44:44 -0600

    Empezando a trabajar con Arrays

---
+++ arreglos.php
@@ -0,0 +1,15 @@
+<?php
+
+// Comenzando con arreglos
+$edades = [20, 18, 40, 30];
+$edades2 = array(20, 18, 40, 50);
+
+// Template strings
+echo "Una de las edades es: $edades[1] \n";
+
+// Concatenacion con .
+echo "Una de las edades es: " . $edades2[3];
+
+echo "\n";
+
+?>
