2d7ec062158649be5c9a5e2a947d501d51e8d720
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-24 11:28:01 -0600

    Ciclo while

---
+++ while.php
@@ -0,0 +1,14 @@
+<?php
+
+// Un bucle es una estructura de control que nos ayuda a repetir un bloque de codigo cierto numero de veces
+
+
+$iterador = 50;
+
+while($iterador > 0) {
+    echo "El numero actual es: $iterador \n";
+    $iterador--;
+};
+
+
+?>
